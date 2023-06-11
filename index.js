d3bc5f2ac8742aea019829317e75eef47cf6f92b
Wade Desco <descowade@gmail.com>
2023-06-11 16:27:50 -0700

    Format

--- index.js
+++ index.js
@@ -3,44 +3,44 @@ const { writeSvgFile } = require('./lib/fileWriter');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
 async function generateLogo() {
-  const userInput = await promptUser();
+    const userInput = await promptUser();
 
-  const shapeColor = userInput.shapeColor;
-  const textColor = userInput.textColor;
-  const text = userInput.text;
-  const shapeType = userInput.shape;
+    const shapeColor = userInput.shapeColor;
+    const textColor = userInput.textColor;
+    const text = userInput.text;
+    const shapeType = userInput.shape;
 
-  let shape;
+    let shape;
 
-  switch (shapeType) {
-    case 'circle':
-      shape = new Circle();
-      break;
-    case 'triangle':
-      shape = new Triangle();
-      break;
-    case 'square':
-      shape = new Square();
-      break;
-    default:
-      console.log('Invalid shape selected.');
-      return;
-  }
+    switch (shapeType) {
+        case 'circle':
+            shape = new Circle();
+            break;
+        case 'triangle':
+            shape = new Triangle();
+            break;
+        case 'square':
+            shape = new Square();
+            break;
+        default:
+            console.log('Invalid shape selected.');
+            return;
+    }
 
-  shape.setColor(shapeColor);
+    shape.setColor(shapeColor);
 
-  // Generate the SVG string
-  const svgString = `
+    // Generate the SVG string
+    const svgString = `
     <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
       ${shape.render()}
       <text x="150" y="120" text-anchor="middle" fill="${textColor}">${text}</text>
     </svg>
   `;
 
-  // Write the SVG string to a file
-  writeSvgFile(svgString);
+    // Write the SVG string to a file
+    writeSvgFile(svgString);
 
-  console.log('Generated logo.svg');
+    console.log('Generated logo.svg');
 }
 
 generateLogo();
